79386b6cf08bda7125f51ec68f9c6882d13b1ce1
Hosein Hojat Ansari <hhojatansari@gmail.com>
2018-08-11 21:35:52 +0430

    add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+class State():
+    def __init__(self):
+        data = {
+            "date" : "00-00-00", "time" : "00:00:00", "M000" : "OFF", "M001" : "OFF", "M002" : "OFF", "M003" : "OFF",
+            "M004": "OFF", "M005" : "OFF", "M006" : "OFF", "M007" : "OFF", "M008" : "OFF", "M009" : "OFF", "M010" : "OFF",
+            "M011": "OFF", "M012": "OFF", "M013": "OFF", "M014": "OFF", "M015": "OFF", "M016": "OFF", "M017": "OFF",
+            "M018": "OFF","M019": "OFF", "M020": "OFF", "M021": "OFF", "M022": "OFF", "M023": "OFF", "M024": "OFF",
+            "M025": "OFF","M026": "OFF", "M027": "OFF",
+        }
